c67eb1df882730d5f8d09d86876586bb56a63fbd
Alexandre <alexandrerichard45@sfr.fr>
2022-08-19 14:46:07 +0200

    page produit fini

--- front/js/product.js
+++ front/js/product.js
@@ -29,82 +29,86 @@ fetch('http://localhost:3000/api/products/' + url)
         i++
       }
       let bouton = document.getElementById('addToCart')
-
       bouton.addEventListener('click', function () {
-        let quantite = parseInt(document.getElementById('quantity').value)
+        let quantiteChoisi = parseInt(document.getElementById('quantity').value)
         let couleurChoisi = document.getElementById('colors').value
-        let tableau = [url, couleurChoisi, quantite]
-        if (quantite >= 1 && quantite <= 100) {/* si le canapé existe dans le panier */
-          if (localStorage.getItem(url + couleurChoisi) !== null && couleurChoisi.length > 0) { /* si la couleur est valide */
-            quantite += parseInt(localStorage.getItem(url + couleurChoisi).split(',')[2])
-            console.log("avant de supprimer" + tableau + '/' + typeof (quantite))
-            tableau.pop()
-            console.log("apres supprimer" + tableau)
-            tableau.push(parseInt(quantite))
-            console.log("fini " + tableau)
-            localStorage.setItem(url + couleurChoisi, tableau)
-            console.log(localStorage)
-            bouton.innerHTML = "Ajouté !"
-          }
-          if (couleurChoisi.length > 0) { /* si la couleur n'existe pas */
-            console.log(tableau)
-            localStorage.setItem(url + couleurChoisi, tableau)
-            console.log(localStorage)
-            bouton.innerHTML = "Ajouté !"
-          } else { /* la couleur n'est pas valide */
-          bouton.innerHTML = "Veuillez sélectionner une couleur"
+        let article = {
+          id: url
         }
-        }else{
-          bouton.innerHTML = "Veuillez sélectionner un nombre valide entre 1 et 100"
+        let tableau = new Array()
+
+        verification()
+
+        function verificationQuantite(nombre) {
+          console.log(nombre)
+          if (quantiteChoisi >= 1 && quantiteChoisi <= 100) {
+            article.quantite = quantiteChoisi
+            return nombre
+          } else {
+            bouton.innerHTML = 'Veuillez sélectionner un nombre valide entre 1 et 100'
+            return false
+          }
+        }
+
+        function verificationCouleur(couleur) {
+          console.log(couleur)
+          if (!!couleur) {
+            article.couleur = couleur
+            return couleurChoisi
+          } else {
+            bouton.innerHTML = 'Veuillez sélectionner une couleur'
+            return false
+          }
+        }
+
+        function verification() {
+          if (verificationCouleur(couleurChoisi) == false && !!verificationQuantite(quantiteChoisi) == false) {
+            bouton.innerHTML = 'Veuillez vérifier les données saisies '
+          }else {
+            if(!!verificationQuantite(quantiteChoisi) == false || !!verificationCouleur(couleurChoisi) == false){
+              verificationQuantite(quantiteChoisi)
+              verificationCouleur(couleurChoisi)
+            }else{
+            condition(couleurChoisi, url)
+            }
+          }
+        }
+
+        function condition(couleur, id) {
+          panierVide()
+          let tableaufiltre = tableau.filter(element => element.couleur == couleur && element.id == id)
+          if (tableaufiltre.length == 0) {
+            enregistrementCanapeAbsence()
+          } else {
+            enregistrementCanapeExistance(tableaufiltre)
+          }
+        }
+
+        function enregistrementCanapeAbsence() {
+          tableau.push(article)
+          localStorage.setItem('panier', JSON.stringify(tableau))
+          bouton.innerHTML = "Ajouté !"
+          console.log(localStorage.getItem('panier'))
+        }
+
+        function enregistrementCanapeExistance(tableaufiltre) {
+          article.quantite = quantiteChoisi + tableaufiltre[0].quantite
+          Object.defineProperty(tableaufiltre[0], 'quantite', {
+            value: article.quantite
+          })
+          localStorage.setItem('panier', JSON.stringify(tableau))
+        }
+
+        function panierVide() {
+          if (!!localStorage.getItem('panier')) {
+            console.log(JSON.parse(localStorage.getItem('panier')))
+            return tableau = JSON.parse(localStorage.getItem('panier'))
+          }
         }
       })
     };
   })
-  .catch(function (err) {
-    console.log(err)
-    // Une erreur est survenue
-  })
-
-
-
-/*   if (quantite >= 1 && quantite <= 100) {      si la quantite est correct a une plage 
-  if (couleurChoisi.length > 0) {          si la couleur est valide 
-    if (url === localStorage.id) {    verifie si il y a l'id dans le localstorage 
-      if (couleurChoisi === localStorage.couleur) {
-        let localQuantite = parseInt(localStorage.getItem('quantite'))
-        quantite += localQuantite     on incrémente la quantite ajouter au panier 
-        localStorage.setItem('quantite', quantite)    on l'ajoute a localStorage 
-        localStorage.setItem('id', url)
-        localStorage.setItem('couleur', couleurChoisi)
-        console.log(tableau)
-      } else {
-        let quantite = parseInt(localStorage.getItem('quantite'))
-        let couleurChoisi = document.getElementById('colors').value
-        localStorage.setItem('quantite', quantite)    on l'ajoute a localStorage
-        localStorage.setItem('id', url)
-        localStorage.setItem('couleur', couleurChoisi)
-        tableau.push(localStorage)
-        console.log(tableau)
-      }
-    } else {
-      localStorage.setItem('id', url)
-      localStorage.setItem('quantite', document.getElementById('quantity').value)
-      localStorage.setItem('couleur', document.getElementById('colors').value)
-      tableau.push(localStorage)
-      console.log(localStorage)
-    }
-  } */
-/*   let tableau = []
-  let bouton = document.getElementById('addToCart')
-  bouton.addEventListener('click', function () {
-    tableau.push('id' , urlid)
-    console.log(tableau)
-    localStorage.setItem('id', urlid)
-    localStorage.setItem('quantite', document.getElementById('quantity').value)
-    let id = localStorage.getItem('id')
-    let quantite = localStorage.getItem('quantite')
-    tableau.push(localStorage)
-    console.log(id + ' ' + quantite)
-    console.log(tableau)
-    bouton.innerHTML = "ajouté !";
-  }); */
+  .catch (function (err) {
+  console.log(err)
+  // Une erreur est survenue
+})
